f629451ec97391eddecc5f4cdf984c2482f6bc33
team4 <michael19971219@gmail.com>
2019-01-31 18:30:50 -0800

    lab14-15 finished

--- applications/scripts/interactive_marker_demo.py
+++ applications/scripts/interactive_marker_demo.py
@@ -6,6 +6,8 @@ from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Quaternion, Pose, Point, Vector3
 from std_msgs.msg import Header, ColorRGBA
 import rospy
+import robot_api
+import math
 
 
 def wait_for_time():
@@ -14,45 +16,73 @@ def wait_for_time():
 	while rospy.Time().now().to_sec() == 0:
 		pass
 
-def handle_viz_input(input):
-	if (input.event_type == InteractiveMarkerFeedback.BUTTON_CLICK):
-		rospy.loginfo(input.marker_name + ' was clicked.')
-	else:
-		rospy.loginfo('Cannot handle this InteractiveMarker event')
+
+
+class Interactive(object):
+	def __init__(self, server, x, y, name):
+		self._server = server
+		self._x = x
+		self._y = y
+		self._name = name
+
+	def make(self):
+		int_marker = InteractiveMarker()
+		int_marker.header.frame_id = "base_link"
+		int_marker.name = self._name
+		int_marker.description = self._name
+		int_marker.pose.position.x = self._x
+		int_marker.pose.position.y = self._y
+		int_marker.pose.orientation.w = 1
+
+		box_marker = Marker()
+		box_marker.type = Marker.CUBE
+		box_marker.pose.orientation.w = 1
+		box_marker.scale.x = 0.45
+		box_marker.scale.y = 0.45
+		box_marker.scale.z = 0.45
+		box_marker.color.r = 0.0
+		box_marker.color.g = 0.5
+		box_marker.color.b = 0.5
+		box_marker.color.a = 1.0
+
+		button_control = InteractiveMarkerControl()
+		button_control.interaction_mode = InteractiveMarkerControl.BUTTON
+		button_control.always_visible = True
+		button_control.markers.append(box_marker)
+		int_marker.controls.append(button_control)
+
+		self._server.insert(int_marker, self.handle_viz_input)
+		self._server.applyChanges()
+
+
+	def handle_viz_input(self, input):
+		base = robot_api.Base()
+		if (input.event_type == InteractiveMarkerFeedback.BUTTON_CLICK):
+			rospy.loginfo(input.marker_name + ' was clicked.')
+			if (input.marker_name == 'forward'): 
+				base.go_forward(0.5)
+			elif (input.marker_name == 'rotate clockwise'):
+				base.turn(-30.0/180*math.pi)
+			elif (input.marker_name == 'rotate counter-clockwise'):
+				base.turn(30.0/180*math.pi)
+				
+
+
 
 
 def main():
 	rospy.init_node('interactive_marker_demo')
 	wait_for_time()
 
+	# marker_publisher = rospy.Publisher('', Marker, queue_size=5)
+
 	server = InteractiveMarkerServer("simple_marker")
-	int_marker = InteractiveMarker()
-	int_marker.header.frame_id = "base_link"
-	int_marker.name = "my_marker"
-	int_marker.description = "Simple Click Control"
-	int_marker.pose.position.x = 1
-	int_marker.pose.orientation.w = 1
-
-	box_marker = Marker()
-	box_marker.type = Marker.CUBE
-	box_marker.pose.orientation.w = 1
-	box_marker.scale.x = 0.45
-	box_marker.scale.y = 0.45
-	box_marker.scale.z = 0.45
-	box_marker.color.r = 0.0
-	box_marker.color.g = 0.5
-	box_marker.color.b = 0.5
-	box_marker.color.a = 1.0
-
-	button_control = InteractiveMarkerControl()
-	button_control.interaction_mode = InteractiveMarkerControl.BUTTON
-	button_control.always_visible = True
-	button_control.markers.append(box_marker)
-	int_marker.controls.append(button_control)
-
-	server.insert(int_marker, handle_viz_input)
-	server.applyChanges()
-
+	marker1 = Interactive(server, 1, 0, 'forward')
+	marker2 = Interactive(server, 0, 1, 'rotate counter-clockwise')
+	marker3 = Interactive(server, 0, -1, 'rotate clockwise')
+	marker1.make()
+	marker2.make()
+	marker3.make()
 	rospy.spin()
 
 
--- robot_api/src/robot_api/base.py
+++ robot_api/src/robot_api/base.py
@@ -2,7 +2,13 @@
 
 import geometry_msgs.msg
 import rospy
+import math
+import copy
+import nav_msgs.msg
+import tf.transformations as tft
+import numpy as np
 
+ANGLELIMIT = 0.01
 
 class Base(object):
     """Base controls the mobile base portion of the Fetch robot.
@@ -16,6 +22,12 @@ class Base(object):
 
     def __init__(self):
         self._pub = rospy.Publisher('cmd_vel', geometry_msgs.msg.Twist, queue_size=5)
+        self._odom_sub = rospy.Subscriber('odom', nav_msgs.msg.Odometry, callback=self._odom_callback)
+        self._odom = None
+
+    def _odom_callback(self, msg):
+        # TODO: do something
+        self._odom = msg.pose.pose
 
     def move(self, linear_speed, angular_speed):
         """Moves the base instantaneously at given linear and angular speeds.
@@ -42,3 +54,94 @@ class Base(object):
         """
         # TODO: Publish 0 velocity
         self.move(0,0)
+
+    def go_forward(self, distance, speed=0.1):
+        """Moves the robot a certain distance.
+
+        It's recommended that the robot move slowly. If the robot moves too
+        quickly, it may overshoot the target. Note also that this method does
+        not know if the robot's path is perturbed (e.g., by teleop). It stops
+        once the distance traveled is equal to the given distance or more.
+
+        Args:
+            distance: The distance, in meters, to move. A positive value
+                means forward, negative means backward.
+            speed: The speed to travel, in meters/second.
+        """
+        # TODO: rospy.sleep until the base has received at least one message on /odom
+        # TODO: record start position, use Python's copy.deepcopy
+        while self._odom is None:
+            rospy.sleep(0.1)
+
+        start = copy.deepcopy(self._odom)
+        rate = rospy.Rate(10)
+        # TODO: CONDITION should check if the robot has traveled the desired distance
+        # TODO: Be sure to handle the case where the distance is negative!
+        distanceCur = math.sqrt((start.position.x - self._odom.position.x)**2 + (start.position.y - self._odom.position.y)**2)
+        while distanceCur < distance:
+            # TODO: you will probably need to do some math in this loop to check the CONDITION
+            direction = -1 if distance < 0 else 1
+            self.move(direction * speed, 0)
+            rate.sleep()
+            distanceCur = math.sqrt((start.position.x - self._odom.position.x)**2 + (start.position.y - self._odom.position.y)**2)
+
+    def turn(self, angular_distance, speed=0.5):
+        """Rotates the robot a certain angle.
+
+        Args:
+            angular_distance: The angle, in radians, to rotate. A positive
+                value rotates counter-clockwise.
+            speed: The angular speed to rotate, in radians/second.
+        """
+        while self._odom is None:
+            rospy.sleep(0.1)
+        # TODO: record start position, use Python's copy.deepcopy
+        start = copy.deepcopy(self._odom)
+        direction = -1 if angular_distance < 0 else 1
+        mat = tft.quaternion_matrix([self._odom.orientation.x, self._odom.orientation.y, self._odom.orientation.z, self._odom.orientation.w])
+        angleCur = math.atan2(mat[1, 0], mat[0, 0])
+        angleEnd = (angleCur + angular_distance)
+        print(str(angleCur) + "    " + str(angleEnd))
+        if math.fabs(angleEnd) > math.pi:
+            angleEnd -= direction*(2*math.pi)
+        # if angleEnd > math.pi:
+        #     angleEnd -= (2*math.pi)
+        # if angleEnd < -math.pi:
+        #     angleEnd += (2*math.pi)
+        rate = rospy.Rate(10)
+        # TODO: CONDITION should check if the robot has rotated the desired amount
+        # TODO: Be sure to handle the case where the desired amount is negative!
+        while not self.reachGoal(angleCur, angleEnd, direction):
+            # TODO: you will probably need to do some math in this loop to check the CONDITION
+            self.move(0, direction * speed)
+            rate.sleep()    
+            mat = tft.quaternion_matrix([self._odom.orientation.x, self._odom.orientation.y, self._odom.orientation.z, self._odom.orientation.w])
+            angleCur = math.atan2(mat[1, 0], mat[0, 0])
+
+    def reachGoal(self, angleCur, angleEnd, direction):
+        print(str(angleCur) + "    " + str(angleEnd))
+        if angleCur*angleEnd < 0:
+            return False
+        if direction > 0:
+            angleCur += ANGLELIMIT;
+            if angleEnd > 0:
+                if angleCur > angleEnd:
+                    return True
+            else:
+                if angleCur > angleEnd:
+                    return True
+        else:
+            angleCur -= ANGLELIMIT;
+            if angleEnd > 0:
+                if angleCur < angleEnd:
+                    return True
+            else:
+                if angleCur < angleEnd:
+                    return True
+
+        return False
+
+
+
+
+
