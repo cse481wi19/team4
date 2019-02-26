adbc83cff9a1fe3e87a0e687b741a4fc2b4588de
team4 <michael19971219@gmail.com>
2019-02-26 12:24:54 -0800

    face

--- face_recognition.py
+++ face_recognition.py
@@ -50,11 +50,7 @@ while True:
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
+    
     # Display the resulting image
     cv2.imshow('Video', frame)
 
